644da8bf4519bf00549263589767166159ba2ef7
George <george.willis@eloquent-technologies.com>
2019-03-15 10:47:28 +0000

    sfgw

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
+use log;
 
 class DeployController extends Controller
 {
@@ -15,7 +16,12 @@ class DeployController extends Controller
      $localToken = config('app_deploy_secret');
      $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
 
+     
+    
+     
+
      if (hash_equals($githubHash, $localHash)) {
+      Log::debug('in yay');
           $root_path = base_path();
           $process = new Process('cd ' . $root_path . '; ./deploy.sh');
           $process->run(function ($type, $buffer) {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Changessf
+                  23234
                 </div>
 
                
