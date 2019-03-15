f8f71a413b90bd7b504319386ed54764ef0a8ebc
George <george.willis@eloquent-technologies.com>
2019-03-15 11:06:10 +0000

    we

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -17,17 +17,16 @@ class DeployController extends Controller
      $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
 
      Log::debug($localHash);
-     
      Log::debug($githubHash);
      
 
-     if (hash_equals($githubHash, $localHash)) {
+     //if (hash_equals($githubHash, $localHash)) {
       
           $root_path = base_path();
           $process = new Process('cd ' . $root_path . '; ./deploy.sh');
           $process->run(function ($type, $buffer) {
               echo $buffer;
           });
-     }
+     //}
   }
 }
