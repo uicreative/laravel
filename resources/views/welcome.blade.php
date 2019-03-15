1943c5657a9d05364e19aeffb2dd7d32408817d0
George <george.willis@eloquent-technologies.com>
2019-03-15 11:10:40 +0000

    we

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Working!
+                    HELLO WORLD!
                 </div>
 
                
