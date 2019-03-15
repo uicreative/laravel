3ad7bb9bee34cadcc1fc94fcab1737974d388953
George <george.willis@eloquent-technologies.com>
2019-03-15 11:19:09 +0000

    wertg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello George!
+                    George!
                 </div>
 
                
